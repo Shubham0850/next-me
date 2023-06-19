567ac859147dfa1649a7c61576dd38b92ef44286
Shubham0850 <rajshubham0850@gmail.com>
2023-06-19 19:10:17 +0530

    header updated and refactor

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import Me from "../public/sqr-logo.png";
+import Me from "../public/me-vector.svg";
 import PlaySound from "./PlaySound";
 import { Zoom, Fade } from "react-reveal";
 
 import About from "./About";
+import { HoverButton } from "../utils/animation";
 
 export default function Header() {
   const [about, setAbout] = useState(false);
+  const btnRef1 = useRef(null);
+
+  useEffect(() => {
+    const btn1 = btnRef1.current;
+
+    new HoverButton(btn1);
+  }, []);
 
   const showAbout = () => {
     if (about) {
@@ -19,20 +27,21 @@ export default function Header() {
 
   return (
     <header className="header">
-      <Zoom bottom>
+      <button className="header__circle" ref={btnRef1}>
         <div className="header__img">
           <Image src={Me} alt="Shubham photo" title="It's me" />
         </div>
-      </Zoom>
+      </button>
 
       <Fade bottom>
-        <h2 className="heading-secondary">Hi👋, I am Shubham Raj (Gulzari)</h2>
+        <h2 className="heading-secondary">Hiya! 👋 Shubham Raj, aka Gulzari</h2>
       </Fade>
 
       <Zoom bottom>
         <h1 className="heading-primary">
-          Self taught📚 WE₿3 Developer👨‍💻
-          <br />& Software Engineer🎓
+          👩‍🚀 The Frontend Speedster 🚀,
+          <br /> 🏎️ Master of Fast Website Wizardry 💨
+          <br /> 🏔️Turning pixels into pure magic!🪄
         </h1>
       </Zoom>
 
